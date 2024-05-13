5fe0f88dfb33215f8ed21e090850bc593631c685
devloper-xps <ashish.tomar@yellowstonexps.com>
2024-05-13 15:24:52 +0530

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/brokenribs/app/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.brokenribs.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.brokenribs.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String AUTH = "563492ad6f91700001000001466190131bcc4dc182d0d9332def2308";
+  public static final String AUTH_KEY = "Authorization";
+}
